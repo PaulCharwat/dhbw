9d9294d29397b6105de7a520d3a6e640d2e05d13
Paul Charwat <paul.charwat@icloud.com>
2022-11-09 16:35:02 +0100

    rename class

--- src/de/paulcharwat/java1/chapter8/Vektor.java
+++ src/de/paulcharwat/java1/chapter8/Vektor.java
@@ -6,30 +6,30 @@ public class Vektor {
 	private int y;
 	private int z;
 	
-	public DreidimensionaleVektoren(int px, int py,int pz) {
+	public Vektor(int px, int py,int pz) {
 		x = px;
 		y = py;
 		z = pz;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(new DreidimensionaleVektoren(3, 8, 1).addieren(new DreidimensionaleVektoren(1, 5, 8)).toString());
-		System.out.println(new DreidimensionaleVektoren(3, 8, 1).subtrahieren(new DreidimensionaleVektoren(1, 5, 8)).toString());
-		System.out.println(new DreidimensionaleVektoren(3, 8, 1).multiplizieren(new DreidimensionaleVektoren(1, 5, 8)));
-		System.out.println(new DreidimensionaleVektoren(3, 8, 1).kreuzprodukt(new DreidimensionaleVektoren(1, 5, 8)).toString());
+		System.out.println(new Vektor(3, 8, 1).addieren(new Vektor(1, 5, 8)).toString());
+		System.out.println(new Vektor(3, 8, 1).subtrahieren(new Vektor(1, 5, 8)).toString());
+		System.out.println(new Vektor(3, 8, 1).multiplizieren(new Vektor(1, 5, 8)));
+		System.out.println(new Vektor(3, 8, 1).kreuzprodukt(new Vektor(1, 5, 8)).toString());
 	}
 	
-	public DreidimensionaleVektoren addieren(DreidimensionaleVektoren dreidimesionaleVektoren) {
-		return new DreidimensionaleVektoren(x+dreidimesionaleVektoren.x, y+dreidimesionaleVektoren.y, z+dreidimesionaleVektoren.z);
+	public Vektor addieren(Vektor dreidimesionaleVektoren) {
+		return new Vektor(x+dreidimesionaleVektoren.x, y+dreidimesionaleVektoren.y, z+dreidimesionaleVektoren.z);
 	}
-	public DreidimensionaleVektoren subtrahieren(DreidimensionaleVektoren dreidimesionaleVektoren) {
-		return new DreidimensionaleVektoren(x-dreidimesionaleVektoren.x, y-dreidimesionaleVektoren.y, z-dreidimesionaleVektoren.z);
+	public Vektor subtrahieren(Vektor dreidimesionaleVektoren) {
+		return new Vektor(x-dreidimesionaleVektoren.x, y-dreidimesionaleVektoren.y, z-dreidimesionaleVektoren.z);
 	}	
-	public int multiplizieren(DreidimensionaleVektoren dreidimensionaleVektoren) {
-		return (x*dreidimensionaleVektoren.x + y*dreidimensionaleVektoren.y + z*dreidimensionaleVektoren.z);
+	public int multiplizieren(Vektor Vektor) {
+		return (x*Vektor.x + y*Vektor.y + z*Vektor.z);
 	}
-	public DreidimensionaleVektoren kreuzprodukt(DreidimensionaleVektoren dreidimesionaleVektoren) {
-		return new DreidimensionaleVektoren(y+dreidimesionaleVektoren.z - z*dreidimesionaleVektoren.y, z*dreidimesionaleVektoren.x - x*dreidimesionaleVektoren.z, x*dreidimesionaleVektoren.y - y*dreidimesionaleVektoren.x);
+	public Vektor kreuzprodukt(Vektor dreidimesionaleVektoren) {
+		return new Vektor(y+dreidimesionaleVektoren.z - z*dreidimesionaleVektoren.y, z*dreidimesionaleVektoren.x - x*dreidimesionaleVektoren.z, x*dreidimesionaleVektoren.y - y*dreidimesionaleVektoren.x);
 	}
 	public String toString() {
         return "(" + x + "|"+ y + "|"+ z + ")";
